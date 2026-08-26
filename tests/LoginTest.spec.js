f8c399ce0a7c1e9e08f3804b0ca6fbee4ea59ef9
Sambari Akhila (Omnicom) <sambari.akhila@omc.com>
2026-08-26 22:21:22 +0530

    Added login Test

---
+++ tests/LoginTest.spec.js
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env',
+  override: true
+});
+
+test('Login Test', async ({ page }) => {
+    await page.goto('https://smarterp-wgaw.onrender.com/');
+    await page.fill('#username', process.env.USERNAME);
+    console.log("uname =", process.env.USERNAME);
+    await page.fill('#password', process.env.PASSWORD);
+    console.log("pwd =", process.env.PASSWORD);
+    await page.click('#loginBtn');
+    console.log("Logged in successfully");
+
+});
